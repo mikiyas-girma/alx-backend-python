57e4cf9235646948260f6e6a73203105d803eeaa
Mikiyas Girma <mikiyasgirmaet@gmail.com>
2024-08-01 11:27:45 +0300

    Basic annotation 1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+    simple type annotated function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ adds two float numbers and return the result """
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,3 @@
+# alx-backend-python
+
+This directory contains a Python code with type annotations.
